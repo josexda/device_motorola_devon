a5163ae1100dea4bba0378e913c5f9089a3b0c32
josexda <josegustavo6@protonmail.com>
2025-12-15 19:11:34 -0300

    devon: pixelos bringup
    Change-Id: I1cf94e7b404ed67d118210d0bf7e44e5a9a599bd

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,4 +4,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_devon.mk
+    $(LOCAL_DIR)/custom_devon.mk
--- custom_devon.mk
+++ custom_devon.mk
@@ -10,11 +10,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/motorola/devon/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_devon
+PRODUCT_NAME := custom_devon
 PRODUCT_DEVICE := devon
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g32
